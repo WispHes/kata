e81db14e63f40b98c258215bc632b27d10a28412
Dmitry Ostapchuk <95207376+WispHes@users.noreply.github.com>
2024-01-27 22:56:12 +0300

    Update main.go

--- main.go
+++ main.go
@@ -20,17 +20,25 @@ func parsSign(a string) string {
 	}
 	panic("На вход поступил неверный арифметический знак")
 }
+
 func getNums(x, y, s string) interface{} {
 	if strings.ContainsAny(rome, x) && strings.ContainsAny(rome, y) {
 		return parsRome(x, y, s)
 	}
 	first, err1 := strconv.Atoi(x)
 	second, err2 := strconv.Atoi(y)
+	checkTrueNum(first)
+	checkTrueNum(second)
 	if err1 != nil || err2 != nil {
 		panic("Ожидаются только арбские либо только рисмские числа которые не больше 10")
 	}
-
 	return getOp(first, second, s)
+}
+
+func checkTrueNum(n int) {
+	if n < 1 || n > 10 {
+		panic("Ожидаются только арбские либо только рисмские числа которые не больше 10")
+	}
 }
 
 func parsRome(x, y, s string) string {
@@ -38,7 +46,7 @@ func parsRome(x, y, s string) string {
 	r, ok2 := numRome[y]
 	if !ok1 || !ok2 {
 		panic("Ожидаются только арбские либо только рисмские числа которые не больше 10")
-	} else if (l - r) < 0 {
+	} else if s == "-" && (l-r) < 0 {
 		panic("В римской системе нет отрицательных чисел.")
 	}
 	num := getOp(l, r, s)
@@ -72,9 +80,9 @@ func getOp(x, y int, s string) int {
 
 func main() {
 	var x, s, y string
-	_, err := fmt.Scan(&x, &s, &y)
+	_, err := fmt.Scanln(&x, &s, &y)
 	if err != nil {
-		return
+		panic("Ожидается два операнда")
 	}
 	sign := parsSign(s)
 	fmt.Println(getNums(x, y, sign))
